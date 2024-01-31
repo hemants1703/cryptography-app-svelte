4c89159589420d0a57ca57e72362be059c89b272
Hemant Sharma <itshemantsharma173@gmail.com>
2024-01-31 20:00:37 +0530

    reset vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// base: process.env.NODE_ENV === 'production'
-	// 	? '/cryptography-app-svelte/tree/gh-pages'
-	// 	: "/",
-	base: "/",
 	plugins: [sveltekit()],
 });
