17dc5cb4f49c06387ce1cb770a485f507cedd2a6
Hemant Sharma <itshemantsharma173@gmail.com>
2024-01-31 17:38:40 +0530

    refactored project

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production'
-		? '/cryptography-app-svelte/tree/gh-pages'
-		: "/",
+	// base: process.env.NODE_ENV === 'production'
+	// 	? '/cryptography-app-svelte/tree/gh-pages'
+	// 	: "/",
+	base: "/",
 	plugins: [sveltekit()],
 });
