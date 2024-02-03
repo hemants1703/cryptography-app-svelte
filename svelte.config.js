1f34ca5d5a481d3ace221a1434af42e16148299f
Hemant Sharma <itshemantsharma173@gmail.com>
2024-02-03 14:24:31 +0530

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
